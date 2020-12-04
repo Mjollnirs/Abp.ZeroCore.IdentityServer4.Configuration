4c18f888dcfe897a0c108c56e697eb42af448ba3
Michael He <yxx1988521@qq.com>
2020-12-04 11:31:58 +0800

    Framework update to .net 5.0. Abp update to 6.0 Add IdentityServer4.vNext Library

---
+++ src/Abp.ZeroCore.IdentityServer4.vNext.Configuration.EntityFramework/Extensions/IdentityServerBuilderExtensions.cs
@@ -0,0 +1,45 @@
+﻿// Project: Abp.ZeroCore.IdentityServer4.Configuration.EntityFramework
+// File: IdentityServerBuilderExtensions.cs
+// 
+// Copyright 2018 Mjollnir<mjollnir@59k.org>
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Abp.IdentityServer4vNext.Services;
+using Abp.IdentityServer4vNext.Stores;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Abp.IdentityServer4vNext.Extensions
+{
+    /// <summary>
+    /// Extension methods to add Abp Repository support to IdentityServer.
+    /// </summary>
+    public static class IdentityServerBuilderExtensions
+    {
+        /// <summary>
+        /// Configures Repository implementation of IClientStore, IResourceStore, and ICorsPolicyService with IdentityServer.
+        /// </summary>
+        /// <typeparam name="TDbContext">The IAbpConfigurationDbContext to use.</typeparam>
+        /// <param name="builder">The builder.</param>
+        /// <returns></returns>
+        public static IIdentityServerBuilder AddConfigurationStore<TDbContext>(this IIdentityServerBuilder builder)
+            where TDbContext : IAbpConfigurationDbContext
+        {
+            builder.AddClientStore<ClientStore>();
+            builder.AddResourceStore<ResourceStore>();
+            builder.AddCorsPolicyService<CorsPolicyService>();
+
+            return builder;
+        }
+    }
+}
---
+++ src/Abp.ZeroCore.IdentityServer4.vNext.Configuration/Entities/Client.cs
@@ -0,0 +1,74 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE-IdentityServer4-EntityFramework in the project root for license information.
+
+// Project: Abp.ZeroCore.IdentityServer4.Configuration
+// File: Client.cs
+// 
+// Copyright 2018 Mjollnir<mjollnir@59k.org>
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Collections.Generic;
+using Abp.Domain.Entities;
+using IdentityServer4.Models;
+using static IdentityServer4.IdentityServerConstants;
+
+namespace Abp.IdentityServer4vNext.Entities
+{
+    public class Client : Entity<int>
+    {
+        public bool Enabled { get; set; } = true;
+        public string ClientId { get; set; }
+        public string ProtocolType { get; set; } = ProtocolTypes.OpenIdConnect;
+        public List<ClientSecret> ClientSecrets { get; set; }
+        public bool RequireClientSecret { get; set; } = true;
+        public string ClientName { get; set; }
+        public string Description { get; set; }
+        public string ClientUri { get; set; }
+        public string LogoUri { get; set; }
+        public bool RequireConsent { get; set; } = true;
+        public bool AllowRememberConsent { get; set; } = true;
+        public bool AlwaysIncludeUserClaimsInIdToken { get; set; }
+        public List<ClientGrantType> AllowedGrantTypes { get; set; }
+        public bool RequirePkce { get; set; }
+        public bool AllowPlainTextPkce { get; set; }
+        public bool AllowAccessTokensViaBrowser { get; set; }
+        public List<ClientRedirectUri> RedirectUris { get; set; }
+        public List<ClientPostLogoutRedirectUri> PostLogoutRedirectUris { get; set; }
+        public string FrontChannelLogoutUri { get; set; }
+        public bool FrontChannelLogoutSessionRequired { get; set; } = true;
+        public string BackChannelLogoutUri { get; set; }
+        public bool BackChannelLogoutSessionRequired { get; set; } = true;
+        public bool AllowOfflineAccess { get; set; }
+        public List<ClientScope> AllowedScopes { get; set; }
+        public int IdentityTokenLifetime { get; set; } = 300;
+        public int AccessTokenLifetime { get; set; } = 3600;
+        public int AuthorizationCodeLifetime { get; set; } = 300;
+        public int? ConsentLifetime { get; set; } = null;
+        public int AbsoluteRefreshTokenLifetime { get; set; } = 2592000;
+        public int SlidingRefreshTokenLifetime { get; set; } = 1296000;
+        public int RefreshTokenUsage { get; set; } = (int)TokenUsage.OneTimeOnly;
+        public bool UpdateAccessTokenClaimsOnRefresh { get; set; }
+        public int RefreshTokenExpiration { get; set; } = (int)TokenExpiration.Absolute;
+        public int AccessTokenType { get; set; } = (int)0; // AccessTokenType.Jwt;
+        public bool EnableLocalLogin { get; set; } = true;
+        public List<ClientIdPRestriction> IdentityProviderRestrictions { get; set; }
+        public bool IncludeJwtId { get; set; }
+        public List<ClientClaim> Claims { get; set; }
+        public bool AlwaysSendClientClaims { get; set; }
+        public string ClientClaimsPrefix { get; set; } = "client_";
+        public string PairWiseSubjectSalt { get; set; }
+        public List<ClientCorsOrigin> AllowedCorsOrigins { get; set; }
+        public List<ClientProperty> Properties { get; set; }
+    }
+}
